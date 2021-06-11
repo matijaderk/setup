5984b49ff6ca8e4863d6dd3752876e16fb944140
Matija Derk <matijaderk@users.noreply.github.com>
2021-06-11 02:44:55 +0200

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -44,7 +44,7 @@ wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.
 sudo dpkg -i packages-microsoft-prod.deb
 
 sudo apt-get update
-sudo apt-get install ddotnet-sdk-5.0 -y
+sudo apt-get install dotnet-sdk-5.0 -y
 
 # 2.4. Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
